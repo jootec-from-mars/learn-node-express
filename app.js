1d8b1c60ba196192a97749d9f1e17da63d64bd92
James Moorcroft <james.moorcroft@googlemail.com>
2018-11-27 13:47:50 +0000

    End of Routing Chapter

--- app.js
+++ app.js
@@ -13,7 +13,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views/index.html')));
+const nav = [
+  { link: '/books', title: 'Books' },
+  { link: '/authors', title: 'Authors' },
+];
+const bookRouter = require('./src/routes/bookRoutes')(nav);
+
+app.use('/books', bookRouter);
+
+app.get('/', (req, res) => {
+  res.render(
+    'index',
+    {
+      nav,
+      title: 'Library',
+    },
+  );
+});
 
 app.listen(port, () => debug(`Listening at port ${chalk.green(port)}`));
